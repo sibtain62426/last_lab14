0ca722d34c9963a7255cf0a53994fd016ad9c75f
sibtain62426 <64125915+sibtain62426@users.noreply.github.com>
2020-06-15 21:47:22 +0500

    Add files via upload

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using Lab14_Code_1st_.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Lab14_Code_1st_.DAL
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext():base("SchoolContext")
+        { 
+        
+        }
+       public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
